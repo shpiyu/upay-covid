37fcab8496a76764ebf784312cdffb941977bb8c
ajaythapliyal <ajaythapliyal1703@gmail.com>
2021-04-24 23:02:00 +0530

    First commit for helpline component

---
+++ src/app/helpline/helpline.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'helpline',
+  templateUrl: './helpline.component.html',
+  styleUrls: ['./helpline.component.css'],
+})
+export class HelplineComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
